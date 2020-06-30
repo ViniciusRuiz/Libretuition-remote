0929eab76e319f1f07388bae4ba4657fc67d2803
Hasindu Lanka <35098511+HasinduLanka@users.noreply.github.com>
2020-06-30 10:59:46 +0530

    Update liberate.sh

--- liberate.sh
+++ liberate.sh
@@ -2,11 +2,12 @@
 
 inf=i.$1
 outf=o.$1
-wget -c $2 -O $inf
-HandBrakeCLI --preset-import-file Libr.json --preset LibreTuition -i $inf -o $outf
+wget -c $3 -O $inf
+HandBrakeCLI --preset-import-file handbrake.preset.json --preset LibreTuition -i $inf -o $outf
 
 sudo rm $inf
 srv=$(curl -s https://apiv2.gofile.io/getServer | sed -E 's/.*?"server":"(.*?)".*?/\1/g')
-curl --form "filesUploaded=@$outf;type=video/mp4" --form password=letslearn https://"$srv".gofile.io/upload >> $outf.txt
+curl --form "filesUploaded=@$outf;type=video/mp4" --form password=$2 https://"$srv".gofile.io/upload >> $1.log
 sudo rm $outf
+cat $1.log
 
