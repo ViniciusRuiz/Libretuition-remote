53ed8ff1a8659591cc4d68f13e93da55c2a5a09c
Hasindu Lanka <35098511+HasinduLanka@users.noreply.github.com>
2020-07-08 14:07:47 +0530

    Install split

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install curl wget handbrake-cli
+sudo apt-get install -y curl wget handbrake-cli gpac
 sudo chmod +x liberate.sh
 sudo chmod +x downpress.sh
+sudo chmod +x downpressplit.sh
 
 echo Lets liberate
 echo 
