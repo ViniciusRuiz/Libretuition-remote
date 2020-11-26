1a037265aa4282753d29144b732f0f91b79bbdc6
hasindulanka <hlrathnayaka@gmail.com>
2020-11-26 14:52:41 +0530

    install chown fix

--- install.sh
+++ install.sh
@@ -4,7 +4,16 @@ sudo apt-get update
 sudo apt-get install -y curl wget handbrake-cli gpac ffmpeg screen nano
 
 chmod +x *.sh
-sudo chown $USER ./
 
-echo Lets liberate
-echo 
+if [ -z "$1" ]
+then
+    echo "Usage : ./install USERNAME"
+else
+    sudo chown $1 ./
+
+    echo Welcome $1
+    echo Lets liberate
+    echo 
+fi
+
+
