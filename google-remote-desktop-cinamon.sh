8e676c80044fbf8d41a3cc7a543625f763d19369
hasindulanka <hlrathnayaka@gmail.com>
2020-11-26 17:49:09 +0530

    cinamon

--- google-remote-desktop-cinamon.sh
+++ google-remote-desktop-cinamon.sh
@@ -6,12 +6,11 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 
-sudo DEBIAN_FRONTEND=noninteractive \
-    apt install --assume-yes xfce4 desktop-base
-    
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 
-sudo apt install --assume-yes xscreensaver
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install --assume-yes cinnamon-core desktop-base
+
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/cinnamon-session-cinnamon2d" > /etc/chrome-remote-desktop-session'
 
 sudo systemctl disable lightdm.service
 
@@ -21,6 +20,5 @@ sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 
 sudo usermod -a -G chrome-remote-desktop $USER
-
 
 echo Now restart the VM and goto https://remotedesktop.google.com/headless where you can get the remote command
